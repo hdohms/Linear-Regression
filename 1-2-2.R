677c2bad5dc0af38d4816166df69bb464be394a4
hdohms <hdohms@icloud.com>
2018-11-27 22:15:05 +0100

    End of lesson: 1.2.2 Correlation Coefficient and clean up old files

--- 1-2-2.R
+++ 1-2-2.R
@@ -1,21 +1,10 @@
 data("GaltonFamilies")
 
-ds_theme_set()
-
 # Prepare the data set with the heights of fathers and the first son
 galton_heights <- GaltonFamilies %>%
   filter(childNum == 1 & gender == 'male') %>%
   select(father, childHeight) %>%
   rename(son = childHeight)
-
-# Summarize the data
-galton_heights %>% 
-  summarize(mean(father), sd(father), mean(son), sd(son))
-
-# Making the plot
-galton_heights %>%
-  ggplot(aes(father, son)) +
-  geom_point(alpha = 0.5)
 
 # Correlation coefficient of father-son
 galton_heights <- GaltonFamilies %>%
@@ -23,8 +12,3 @@ galton_heights <- GaltonFamilies %>%
   select(father, childHeight) %>%
   rename(son = childHeight)
 galton_heights %>% summarize(cor(father, son))
-
-
-
-        
-
--- Base-on-Balls-or-Stolen-Base.R
+++
@@ -1,7 +0,0 @@
-data("Teams")
-ds_theme_set()
-Teams %>%
-  filter(yearID %in% 1961:2001) %>%
-  mutate(HR_per_game = HR / G, R_per_game = R / G) %>%
-  ggplot(aes(HR_per_game, R_per_game)) +
-  geom_point(alpha = 0.5)
